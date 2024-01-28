cd89f7532488f29a7e2551c8cccf1617c3549461
Oluwapese Alo <oluwapese.alo@github.com>
2024-01-28 13:52:12 +0100

    Mr Pius Assignment

--- hms-apps/identity/src/app.module.ts
+++ hms-apps/identity/src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    // url: 'mysql://root:root@localhost:3303/apoowo',
+    port: 5432,
+    username: 'postgres',
+    password: 'postgres',
+    database: 'newusers',
+    synchronize: true,
+    autoLoadEntities: true
+    // entities: ["dist/**/*.entity{.ts,.js}"],
+  }),UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- hms-apps/identity/src/users/entities/user.entity.ts
+++ hms-apps/identity/src/users/entities/user.entity.ts
@@ -1,1 +1,36 @@
-export class User {}
+  import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+  @Entity('_user')
+  export class User {
+      @PrimaryGeneratedColumn("uuid")
+      id: string;
+  
+      @Column({ unique: true })
+      primaryEmailAddress: string;
+  
+      @Column()
+      firstName: string;
+  
+      @Column({ nullable: true })
+      middleName?: string | undefined;
+  
+      @Column()
+      lastName: string;
+  
+      @Column()
+      passwordHash: string;
+  
+      @Column()
+      backupEmailAddress: string;
+  
+      @Column({ nullable: true })
+      // phone: { [key: string]: any } | undefined;
+      phone: string | undefined;
+  
+      @Column({ default: false })
+      isPrimaryEmailAddressVerified: boolean;
+  
+      @Column({ default: false })
+      isBackupEmailAddressVerified: boolean;
+  }
+  
